e91e4731d5af64636d98d16967c87ca34e24fb7a
Puppydied <rdhyuda@gmail.com>
2026-06-04 07:04:19 +0700

    feat: add interactive 3D Lanyard and RotatingText components from ReactBits

---
+++ app/global.d.ts
@@ -0,0 +1,25 @@
+import { Object3DNode, MaterialNode } from '@react-three/fiber';
+import { MeshLineGeometry, MeshLineMaterial } from 'meshline';
+
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
+  }
+}
+
+declare module '*.glb' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module 'meshline' {
+  export const MeshLineGeometry: any;
+  export const MeshLineMaterial: any;
+  export const raycast: any;
+}
